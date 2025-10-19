30de4fa0841e10e6cdd8c85363c62c179bb4315b
Soldann <guotl321@gmail.com>
2025-10-19 17:05:20 +0200

    Make the circles appear and dissapear as you click

--- plotting.py
+++ plotting.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from vggt.utils.load_fn import load_and_preprocess_images
 import mplcursors
+from matplotlib.patches import Circle
 
 def create_interactive_plot(image1, image2, pixel_map_12, pixel_map_21):
     fig = plt.figure(figsize=(10, 5))
@@ -17,16 +18,28 @@ def create_interactive_plot(image1, image2, pixel_map_12, pixel_map_21):
     ax2.set_title('Image 2')
     ax2.axis('off')
 
+    circle_ax1 = Circle((0, 0), radius=1, edgecolor='red', facecolor='none', linewidth=2)
+    circle_ax2 = Circle((0, 0), radius=1, edgecolor='red', facecolor='none', linewidth=2)
+    ax1.add_patch(circle_ax1)
+    ax2.add_patch(circle_ax2)
+
+    circle_ax1.set_visible(False)
+    circle_ax2.set_visible(False)
+
     def on_click(event):
         if event.inaxes == ax1:
             x, y = int(event.xdata), int(event.ydata)
             mapped_point = pixel_map_12[y, x]
-            ax2.plot(mapped_point[0], mapped_point[1], 'ro')
+            circle_ax2.set_center((mapped_point[0], mapped_point[1]))
+            circle_ax2.set_visible(True)
+            circle_ax1.set_visible(False)
             fig.canvas.draw()
         elif event.inaxes == ax2:
             x, y = int(event.xdata), int(event.ydata)
             mapped_point = pixel_map_21[y, x]
-            ax1.plot(mapped_point[0], mapped_point[1], 'ro')
+            circle_ax1.set_center((mapped_point[0], mapped_point[1]))
+            circle_ax1.set_visible(True)
+            circle_ax2.set_visible(False)
             fig.canvas.draw()
 
     fig.canvas.mpl_connect('button_press_event', on_click)
